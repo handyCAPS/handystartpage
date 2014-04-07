b48d075f9794247747ee995dd65a2f701bc637a3
Tim Doppenberg <info@timdoppenberg.nl>
2014-04-07 09:27:31 +0200

    fixed img preview

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,7 @@ module.exports = function(grunt) {
         dest: 'dist/',
         options: {
           process: function(content) {
-            return content.replace(/lib\//g, '' ).replace(/startpage\.concat\.js/g, 'startpage.min.js').replace(/js\/dist/, 'js/').replace(/\.css/, '.min.css');
+            return content.replace(/lib\//g, '' ).replace(/\.concat\.js/g, '.min.js').replace(/js\/dist\//, 'js/').replace(/\.css/, '.min.css');
           }
         }
       },
--- dist/views/footer.php
+++ dist/views/footer.php
@@ -1,6 +1,6 @@
 </div>
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<script src="js//masonry.pkgd.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
 <script src="js/startpage.min.js"></script>
 </body>
 </html>
